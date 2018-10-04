d7eea684fbe5fc8079c93f75b0e5cf6fcc0bd01c
Ant290 <ant290@live.co.uk>
2018-10-04 23:07:48 +0100

    biggest table script update

--- Biggest5Tables.sql
+++ Biggest5Tables.sql
@@ -1,15 +1,7 @@
 SELECT top (5)
     '[' + (OBJECT_SCHEMA_NAME(tables.object_id,db_id()) 
 	+ '].[' + tables.NAME + ']') AS TableName,
-    indexes.name as indexName,
-    sum(partitions.rows) as RowCounts,
-    sum(allocation_units.total_pages) as TotalPages,
-    sum(allocation_units.used_pages) as UsedPages,
-    sum(allocation_units.data_pages) as DataPages,
-    (sum(allocation_units.total_pages) * 8) / 1024 as TotalSpaceMB,
-    (sum(allocation_units.used_pages) * 8) / 1024 as UsedSpaceMB,
-    (sum(allocation_units.data_pages) * 8) / 1024 as DataSpaceMB,
-	GETDATE() AS Datemodified
+    sum(partitions.rows) as RowCounts
 FROM 
     sys.tables tables
 INNER JOIN      
@@ -26,4 +18,4 @@ WHERE
 GROUP BY 
     tables.object_id,tables.NAME, indexes.object_id, indexes.index_id, indexes.name 
 ORDER BY 
-    TotalSpaceMB desc
+    RowCounts desc
